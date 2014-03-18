75f82459ae56ab301c6425420dc6c9fa3f5558cd
tblazhev <tblazhev@users.noreply.github.com>
2014-03-18 15:51:51 +0200

    Added list parser and tests for it a little late commit, so a few things altogether

---
+++ listparser.py
@@ -0,0 +1,47 @@
+import os
+from glob import glob
+
+
+class ListParser():
+    """docstring for ListParser"""
+    def __init__(self):
+        self.__script_path = os.path.dirname(os.path.realpath(__file__))
+        self.__lists_path = self.__script_path + "/lists"
+        self.__common_path = self.__lists_path + "/list_"
+        self.__glob_path = self.__lists_path + "/list_*"
+
+    def get_script_path(self):
+        return self.__script_path
+
+    def get_lists_path(self):
+        return self.__lists_path
+
+    def get_common_path(self):
+        return self.__common_path
+
+    def get_glob_path(self):
+        return self.__glob_path
+
+    def spaces_to_underscores(self, string):
+        return string.replace(" ", "_")
+
+    def create_list(self, list_name):
+        list_name = self.spaces_to_underscores(list_name)
+        file_path = self.__lists_path + "/list_" + list_name
+        try:
+            f = open(file_path, "x")
+            f.close()
+        except IOError:
+            return False
+        return True
+
+    def get_lists(self):
+        files = glob(self.__glob_path)
+        lists = []
+        for f in files:
+            lists.append(f.replace(self.__glob_path[:-1], ""))
+        lists.sort()
+        return lists
+
+    # def show_list(self, list_name):
+    #     file_path = self.li
---
+++ listparsertests.py
@@ -0,0 +1,69 @@
+import unittest
+from listparser import ListParser
+import os
+from glob import glob
+
+
+class ListParserTest(unittest.TestCase):
+    """docstring for ListParserTest"""
+    def setUp(self):
+        self.list_parser = ListParser()
+        self.script_path = os.path.dirname(os.path.realpath(__file__))
+        self.lists_dir_path = self.script_path + "/lists"
+        self.lists_glob_path = self.lists_dir_path + "/list_*"
+        self.test_list_name = "test_list"
+        self.test_list_path = self.lists_dir_path + "/list_" + self.test_list_name
+
+    def test_init(self):
+        self.assertEqual(self.script_path, self.list_parser.get_script_path())
+        self.assertEqual(self.lists_dir_path, self.list_parser.get_lists_path())
+        self.assertEqual(self.lists_glob_path, self.list_parser.get_glob_path())
+
+    def test_spaces_to_underscores(self):
+        filename = "filename with spaces"
+        new_filename = self.list_parser.spaces_to_underscores(filename)
+        new_filename_check = "filename_with_spaces"
+        self.assertEqual(new_filename_check, new_filename)
+
+    def test_create_list(self):
+        self.assertTrue(self.list_parser.create_list(self.test_list_name))
+        files = glob(self.lists_glob_path)
+        self.assertTrue(self.test_list_path in files)
+
+    def test_create_existing_list(self):
+        self.list_parser.create_list(self.test_list_name)
+        self.assertTrue(not self.list_parser.create_list(self.test_list_name))
+
+    def test_create_list_with_spaces(self):
+        list_with_spaces = "name with spaces"
+        list_with_spaces_path = self.lists_dir_path + "/list_name_with_spaces"
+        self.list_parser.create_list(list_with_spaces)
+        files = glob(self.lists_glob_path)
+        self.assertTrue(list_with_spaces_path in files)
+
+    def test_get_lists(self):
+        self.list_parser.create_list("testlist1")
+        self.list_parser.create_list("testlist2")
+        self.list_parser.create_list("testlist3")
+        lists = ["testlist1", "testlist2", "testlist3"]
+        self.assertEqual(lists, self.list_parser.get_lists())
+
+    # def test_show_list(self):
+    #     self.list_parser.create_list(self.test_list_name)
+    #     f = open(self.test_list_path, "w")
+    #     contents = "Tedi - tedi@hackbulgaria.com\nAdrian - adrian@hackbulgaria.com"
+    #     f.write(contents)
+    #     f.close()
+    #     users = contents.split("\n")
+    #     self.assertEqual(users, self.list_parser.show_list(self.test_list_name))
+
+    def tearDown(self):
+        files = glob(self.lists_glob_path)
+        for f in files:
+            try:
+                os.remove(f)
+            except OSError:
+                pass
+
+if __name__ == '__main__':
+    unittest.main()
