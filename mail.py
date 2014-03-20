8248919eb3a5c00c21d24e8a730a430662fe2aee
fyllmax <andi_777@abv.bg>
2014-03-20 12:52:47 +0200

    added mail.py and mail_test.py to mail branch

--- mail.py
+++ mail.py
@@ -102,7 +102,7 @@ class CommandList():
     #     "* show_list <unique_list_identifier> - Prints all people, one person at a line, that are subscribed for the list. The format is: <Name> - <Email>",
     #     "* add <unique_list_identifier> - Starts the procedure for adding a person to a mail list. The program prompts for name and email.",
     #     "* create <list_name> - Creates a new empty list, with the given name."
-    #     "* search_email <email> - Performs a search into all lists to see if the given email is present. Shows all lists, where the email was found.",
+    #     "* search_email <email> - Performs a search into all lists to see if the given email is present . Shows all lists, where the email was found.",
     #     "* merge_lists <list_identifier_1> <list_identifier_2> <new_list_name> - merges list1 and list2 into a new list, with the given name.",
     #     "* export <unique_list_identifier> - Exports the given list into JSON file, named just like the list. All white spaces are replaced by underscores.",
     #     "* exit - this will quit the program"]
--- mail_test.py
+++ mail_test.py
@@ -1,6 +1,7 @@
 # Imports
 import unittest
 from mail import CommandList
+from listparser import ListParser
 # from os import
 
 
@@ -10,16 +11,27 @@ class test_mail(unittest.TestCase):
 
     """Testing the unit"""
 
+    def setUp(self):
+        self.list_parser = ListParser()
+        self.command_list = CommandList(self.list_parser)
+
     def test_parse_command(self):
         self.assertEqual(('hello', 'world'),
-                         CommandList.parse_command("hello world"))
+                         self.command_list.parse_command("hello world"))
 
-    def is_command(self):
-        self.assertEqual(
-            'hello', CommandList.is_command(('hello', 'world'), "hello"))
+    def test_is_command(self):
+        self.assertTrue(
+            True, self.command_list.is_command(('hello', 'world'), "hello"))
 
-    # def test_show_list(self)
-    #      self.assertEqual( ,CommandList.show_list())
+    def test_show_lists(self):
+        pass
+
+    def test_show_list(self):
+        self.assert(
+            True, self.command_list.show_list(2)
+
+
+
 
 
 # Program Run
