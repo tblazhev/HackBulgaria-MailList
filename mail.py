c04c7d2e84ce880a7653337ccfcd8bc2cbb614e1
fyllmax <andi_777@abv.bg>
2014-03-22 15:22:52 +0200

    up to date 22

--- mail.py
+++ mail.py
@@ -65,12 +65,12 @@ class CommandList():
                 "<{}> was not found in the current mailing lists.".format(email))
         return "\n".join(output)
 
-    def update_subscriber(self, list_id, subs_id):
-        list_name = self.all_lists[int(list_id) - 1]
+    # def update_subscriber(self, list_id, subs_id):
+    #     list_name = self.all_lists[int(list_id) - 1]
 
-        new_name = input("enter new name>")
-        new_email = input("enter new email>")
-        self.list_parser.update_subscriber(list_name,  , ,new_name, new_email)
+    #     new_name = input("enter new name>")
+    #     new_email = input("enter new email>")
+    #     self.list_parser.update_subscriber(list_name, , ,new_name, new_email)
 
 # >update_subscriber 2 1
 # Updating: Radoslav Georgeiv - radorado@hackbulgaria.com
@@ -91,16 +91,19 @@ class CommandList():
 
         return "\n".join(menu)
 
-    # def create_help(self):
-    #     HelpMenu = [
-    #         "Here is a full list of commands: ",
-    #         "",
-    #         "* show_lists - Prints all lists to the screen. Each list is assigned with a unique identifier",
-    #         "* show_list <unique_list_identifier> - Prints all people, one person at a line, that are subscribed for the list. The format is: <Name> - <Email>",
-    #         "* add <unique_list_identifier> - Starts the procedure for adding a person to a mail list. The program prompts for name and email.",
-    #         "* create <list_name> - Creates a new empty list, with the given name."
-    #         "* search_email <email> - Performs a search into all lists to see if the given email is present . Shows all lists, where the email was found.",
-    #         "* merge_lists <list_identifier_1> <list_identifier_2> <new_list_name> - merges list1 and list2 into a new list, with the given name.",
-    #         "* export <unique_list_identifier> - Exports the given list into JSON file, named just like the list. All white spaces are replaced by underscores.",
-    #         "* exit - this will quit the program"]
-    #     return "\n".join(HelpMenu)
+    def create_help(self):
+        HelpMenu = [
+            "Here is a full list of commands: ",
+            "",
+            "* show_lists - Prints all lists to the screen. Each list is assigned with a unique identifier",
+          "* show_list <unique_list_identifier> - Prints all people, one person at a line, that are subscribed for the list. The format is: <Name> - <Email>"
+           "* add <unique_list_identifier> - Starts the procedure for adding a person to a mail list. The program prompts for name and email.",
+           "* update_subscriber <unique_list_identifier> <unique_name_identifier> - updates the information for the given subscriber in the given list",
+           "* remove_subscriber <unique_list_identifier> <unique_name_identifier> - Removes the given subscriber from the given list",
+           "* create <list_name> - Creates a new empty list, with the given name.",
+           "* update <unique_list_identifier>  <new_list_name> - Updates the given list with a new name.",
+           "* search_email <email> - Performs a search into all lists to see if the given email is present. Shows all lists, where the email was found.",
+           "* merge_lists <list_identifier_1> <list_identifier_2> <new_list_name> - merges list1 and list2 into a new list, with the given name.",
+           "* export <unique_list_identifier> - Exports the given list into JSON file, named just like the list. All white spaces are replaced by underscores.",
+           "* exit - this will quit the program"]
+        return "\n".join(HelpMenu)
